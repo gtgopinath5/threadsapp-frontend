587be8f79efb7af843f55098eddbf9224133c6a2
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-04 15:16:37 +0530

    commit

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -25,7 +25,7 @@ const PostPage = () => {
 		const getPost = async () => {
 			setPosts([]);
 			try {
-				const res = await fetch(`/api/posts/${pid}`);
+				const res = await fetch(`https://threadsapp-backend.onrender.com/api/posts/${pid}`);
 				const data = await res.json();
 				if (data.error) {
 					showToast("Error", data.error, "error");
