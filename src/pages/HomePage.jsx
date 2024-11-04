e282ee1b61a001f0de73172f3a081f28b43c584c
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-04 15:20:28 +0530

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,17 +10,34 @@ const HomePage = () => {
 	const [posts, setPosts] = useRecoilState(postsAtom);
 	const [loading, setLoading] = useState(true);
 	const showToast = useShowToast();
+
 	useEffect(() => {
 		const getFeedPosts = async () => {
 			setLoading(true);
 			setPosts([]);
+			const token = localStorage.getItem("token"); // Adjust based on where your token is stored
+
 			try {
-				const res = await fetch("https://threadsapp-backend.onrender.com/api/posts/feed");
+				const res = await fetch("https://threadsapp-backend.onrender.com/api/posts/feed", {
+					method: "GET",
+					headers: {
+						'Authorization': `Bearer ${token}`, // Include the token in the headers
+						'Content-Type': 'application/json', // Optional, but good practice
+					},
+				});
 				const data = await res.json();
+
+				if (res.status === 401) {
+					// Handle unauthorized access
+					showToast("Error", "Unauthorized access. Please log in.", "error");
+					return;
+				}
+
 				if (data.error) {
 					showToast("Error", data.error, "error");
 					return;
 				}
+
 				console.log(data);
 				setPosts(data);
 			} catch (error) {
@@ -36,13 +53,11 @@ const HomePage = () => {
 		<Flex gap='10' alignItems={"flex-start"}>
 			<Box flex={70}>
 				{!loading && posts.length === 0 && <h1>Follow some users to see the feed</h1>}
-
 				{loading && (
 					<Flex justify='center'>
 						<Spinner size='xl' />
 					</Flex>
 				)}
-
 				{posts.map((post) => (
 					<Post key={post._id} post={post} postedBy={post.postedBy} />
 				))}
